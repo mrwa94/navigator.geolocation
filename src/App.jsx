5068002d5ec55946b88b5286e7ac59d9eae83bbc
mrwaff4 <mreaff4@gmail.com>
2024-10-02 18:05:34 +0300

    Displaying the City, Country, and Flag

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import axios from "axios";
+import React, { useState, useEffect, Component } from "react";
+import CountryFlag from "react-country-flag";
+
+const App = () => {
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
+  const [address, setAddress] = useState({
+    city: null,
+    country: null,
+    countryCode: null,
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const API_KEY = "5e5535c8a5254ed8a29081fe710d4913"; //  OpenCage API Key
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          //successfully ..
+          const { latitude, longitude } = position.coords;
+          setLocation({ longitude, latitude });
+          axios
+            .get(
+              `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${API_KEY}`
+            )
+
+            .then((response) => {
+              const components = response.data.results[0].components;
+              const country = components.country;
+              const city =
+                components.city || components.town || components.village;
+              const countryCode = components["country_code"].toUpperCase();
+              setAddress({ country, city, countryCode });
+              setLoading(false);
+            });
+        },
+        (error) => {
+          setError(error);
+          setLoading(false);
+        }
+      );
+    } else {
+      setError(" sorry , GeoLocation is not support in your browser . ");
+    }
+  }, [API_KEY]);
+
+  return (
+    <div>
+      {loading ? (
+        <p> Loading .. </p>
+      ) : error ? (
+        <p> error : {error}</p>
+      ) : (
+        <div>
+          <h5>It seem that you are from </h5>
+          <h5>
+            {address.country} , {address.city}
+          </h5>
+          {address.countryCode && (
+            <CountryFlag
+              countryCode={address.countryCode}
+              svg
+              style={{ width: "2em", height: "2em", marginLeft: "10px" }}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="home">This is Home page .</div>;
+};
+
+export default Home;
